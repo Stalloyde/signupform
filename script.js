85e41f4ff844362ba4eb0fddb495d8a5a67ea50c
Stalloyde <stalloyde93@gmail.com>
2022-08-16 22:32:43 +0800

    Refactor code

--- script.js
+++ script.js
@@ -4,25 +4,32 @@ const pwErrorMessage = document.getElementById("pw-error-message");
 const pwCheck1 = document.getElementById("pw-check1");
 const pwCheck2 = document.getElementById("pw-check2");
 
+function pwMismatch () {
+    pwErrorMessage.textContent = "*Passwords do not match";
+    pwErrorMessage.style.color = "red";         
+    pwCheck2.innerHTML = "<img src=crossred.png height=20px width=20px>";
+    pwCheck1.innerHTML = "<img src=crossred.png height=20px width=20px>";
+    password.style.border = "2px solid red";
+    confirmPassword.style.border = "2px solid red";
+}
+
+function pwMatch () {
+    pwErrorMessage.textContent = "*Passwords match";
+    pwErrorMessage.style.color = "green";
+    pwCheck1.innerHTML = "<img src=greencheckicon.png height=20px width=20px>";
+    pwCheck2.innerHTML = "<img src=greencheckicon.png height=20px width=20px>";
+    password.style.border = "";
+    confirmPassword.style.border = "";
+}
 
 // activate validation message only when confirm password input has been filled
 function passwordCheck () {
     if (confirmPassword.value) {
         if (password.value !== confirmPassword.value) {
-            pwErrorMessage.textContent = "*Passwords do not match";
-            pwErrorMessage.style.color = "red";         
-            pwCheck2.innerHTML = "<img src=crossred.png height=20px width=20px>";
-            pwCheck1.innerHTML = "<img src=crossred.png height=20px width=20px>";
-            password.style.border = "2px solid red";
-            confirmPassword.style.border = "2px solid red";
+            pwMismatch();
         } else if 
         (password.value === confirmPassword.value) {
-            pwErrorMessage.textContent = "*Passwords match";
-            pwErrorMessage.style.color = "green";
-            pwCheck1.innerHTML = "<img src=greencheckicon.png height=20px width=20px>";
-            pwCheck2.innerHTML = "<img src=greencheckicon.png height=20px width=20px>";
-            password.style.border = "";
-            confirmPassword.style.border = "";
+            pwMatch();
         };
     }
 }
