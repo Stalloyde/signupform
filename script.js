d6e734a9282844245a0112792640cc9fdcfc85cb
Stalloyde <stalloyde93@gmail.com>
2022-08-16 22:34:07 +0800

    Renamed function

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const pwErrorMessage = document.getElementById("pw-error-message");
 const pwCheck1 = document.getElementById("pw-check1");
 const pwCheck2 = document.getElementById("pw-check2");
 
-function pwMismatch () {
+function displayError () {
     pwErrorMessage.textContent = "*Passwords do not match";
     pwErrorMessage.style.color = "red";         
     pwCheck2.innerHTML = "<img src=crossred.png height=20px width=20px>";
@@ -13,7 +13,7 @@ function pwMismatch () {
     confirmPassword.style.border = "2px solid red";
 }
 
-function pwMatch () {
+function displayValid () {
     pwErrorMessage.textContent = "*Passwords match";
     pwErrorMessage.style.color = "green";
     pwCheck1.innerHTML = "<img src=greencheckicon.png height=20px width=20px>";
@@ -26,10 +26,10 @@ function pwMatch () {
 function passwordCheck () {
     if (confirmPassword.value) {
         if (password.value !== confirmPassword.value) {
-            pwMismatch();
+            displayError();
         } else if 
         (password.value === confirmPassword.value) {
-            pwMatch();
+            displayValid();
         };
     }
 }
